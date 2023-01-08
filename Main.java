3ddab19dc33e90a4245c94f9598c1925a3b83fd7
Maik Puderbach <maikpuderbach@MacBook-Air-von-Maik.fritz.box>
2023-01-08 20:28:20 +0100

    newBranch with Main.java

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+public static void main(String[] args){
+System.out.println("irgendeine Textausgabe aus dem Masterbranch"); 
+System.out.prinln("Textausgabe Nr. 2 aus dem Master!"); 
+} 
+
+}
